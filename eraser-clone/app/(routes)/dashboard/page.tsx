4e97960d3a8f78972cc6e582b064974e2a41c8cf
avi78 <aviternal20@gmail.com>
2024-03-05 23:38:14 +0530

    updates

--- eraser-clone/app/(routes)/dashboard/page.tsx
+++ eraser-clone/app/(routes)/dashboard/page.tsx
@@ -1,11 +1,38 @@
-import React from 'react'
+"use client"
+import React, { useEffect } from 'react'
+import {Button} from '@/components/ui/button'
+import { LogoutLink, useKindeBrowserClient } from '@kinde-oss/kinde-auth-nextjs'
+import { useMutation, useQuery } from 'convex/react'
+import {api} from '@/convex/_generated/api'
 
-const page = () => {
+function Dashboard() {
+
+  const {user}:any = useKindeBrowserClient();
+  const getUser = useQuery(api.user.getUser,{email:user?.email});
+
+  const createUser=useMutation(api.user.createUser);
+  useEffect(() => {
+    if(user){
+      if(getUser == undefined)
+      {
+        createUser({
+          name:user.given_name,
+          email:user.email,
+          image:user.picture
+        }).then(resp=>{
+          console.log(resp)
+        })
+      }
+    }
+  },[user])
+
   return (
     <div>
-      Hello
+      <Button>
+        <LogoutLink>Logout</LogoutLink>
+      </Button>
     </div>
   )
 }
 
-export default page
+export default Dashboard
